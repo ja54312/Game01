3c7fc4143aeb8beafbb4ce6f57d7126003644a4a
jose antonio alatorre chavez <ja54312@MacBook-Pro-de-jose.local>
2024-01-19 08:31:45 -0600

    responsive

--- game.js
+++ game.js
@@ -1,16 +1,34 @@
-const canvas = document.querySelector('#game')
-const game = canvas.getContext('2d')
+const canvas = document.querySelector('#game');
+const game = canvas.getContext('2d');
 
-window.addEventListener('load',startGame)
+let canvasSize;
+let elementsSize;
 
-function startGame () {
-     //game.fillRect(0,50,100,100);
-   //game.clearRect(50,50,50,50);
-   //game.clearRect()
-  //game.clearRect(0,0,50,50);
+window.addEventListener('load', setCanvasSize);
+window.addEventListener('resize', setCanvasSize);
 
-   game.font = '25px Verdana'
-   game.fillStyle = 'purple';
-  game.textAlign = 'center';
-   game.fillText('Platzi', 35, 25);
+function setCanvasSize() {
+  if (window.innerHeight > window.innerWidth) {
+    canvasSize = window.innerWidth * 0.8;
+  } else {
+    canvasSize = window.innerHeight * 0.8;
+  }
+  
+  canvas.setAttribute('width', canvasSize);
+  canvas.setAttribute('height', canvasSize);
+  
+  elementsSize = canvasSize / 10;
+
+  startGame();
+}
+
+function startGame() {
+  console.log({ canvasSize, elementsSize });
+
+  game.font = elementsSize + 'px Verdana';
+  game.textAlign = 'end';
+
+  for (let i = 1; i <= 10; i++) {
+    game.fillText(emojis['X'], elementsSize, elementsSize * i);
+  }
 }
