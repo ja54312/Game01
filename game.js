5f7ae7f4eaf66e73f522cde58b6c02e85aac60d7
JA54312 <ija54312@gmail.com>
2024-01-26 11:53:25 -0600

    array

--- game.js
+++ game.js
@@ -1,11 +1,11 @@
-const canvas = document.querySelector('#game');
-const game = canvas.getContext('2d');
+const canvas = document.querySelector("#game");
+const game = canvas.getContext("2d");
 
 let canvasSize;
 let elementsSize;
 
-window.addEventListener('load', setCanvasSize);
-window.addEventListener('resize', setCanvasSize);
+window.addEventListener("load", setCanvasSize);
+window.addEventListener("resize", setCanvasSize);
 
 function setCanvasSize() {
   if (window.innerHeight > window.innerWidth) {
@@ -13,10 +13,10 @@ function setCanvasSize() {
   } else {
     canvasSize = window.innerHeight * 0.8;
   }
-  
-  canvas.setAttribute('width', canvasSize);
-  canvas.setAttribute('height', canvasSize);
-  
+
+  canvas.setAttribute("width", canvasSize);
+  canvas.setAttribute("height", canvasSize);
+
   elementsSize = canvasSize / 10;
 
   startGame();
@@ -25,10 +25,21 @@ function setCanvasSize() {
 function startGame() {
   console.log({ canvasSize, elementsSize });
 
-  game.font = elementsSize + 'px Verdana';
-  game.textAlign = 'end';
+  game.font = elementsSize + "px Verdana";
+  game.textAlign = "end";
 
-  for (let i = 1; i <= 10; i++) {
-    game.fillText(emojis['X'], elementsSize, elementsSize * i);
+  const map = maps[0];
+  const mapRows = map.trim().split("\n");
+  const mapRowCols = mapRows.map((row) => row.trim().split(""));
+  console.log({ map, mapRows, mapRowCols });
+
+  for (let row = 1; row <= 10; row++) {
+    for (let col = 1; col <= 10; col++) {
+      game.fillText(
+        emojis[mapRowCols[row - 1][col - 1]],
+        elementsSize * col,
+        elementsSize * row
+      );
+    }
   }
 }
